42967215b699fcf6383da4a2ca9998ba05e5a795
Shyam Mohan <95280841+Shyammohan1998@users.noreply.github.com>
2022-02-11 14:41:28 +0530

    Update DBconnection.php

--- DBconnection.php
+++ DBconnection.php
@@ -1,7 +1,7 @@
 <?php 
     $dbhost = "localhost";
     $dbuser = "root";
-    $dbpass = "EnteSql";
+    $dbpass = "";
     $dbname = "MyGuru";
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die("connection failed");
     mysqli_select_db($conn, $dbname);
